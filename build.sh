cc9ef08035c0803fb1563d7f5c8b255d23f6aa23
Mathieu Méa <mathieu.mea@gmail.com>
2018-09-20 10:54:49 -0400

    New build script

--- build.sh
+++ build.sh
@@ -21,31 +21,43 @@ if [ -f ${CUSTOM_SETTINGS_GRADLE_FILE} ]; then
 	SETTINGS_FILE_ARGS=" -c $CUSTOM_SETTINGS_GRADLE_FILE"; #--settings-file
 fi
 
+echo ">> Setup-ing keys...";
 ./keys_setup.sh;
 RESULT=$?;
 checkResult ${RESULT};
+echo ">> Setup-ing keys... DONE";
 
+echo ">> Gradle cleaning...";
 ../gradlew ${SETTINGS_FILE_ARGS} clean ${GRADLE_ARGS};
 RESULT=$?;
 checkResult ${RESULT};
+echo ">> Gradle cleaning... DONE";
 
 if [ ${IS_CI} = true ]; then
+    echo ">> Running lint & test...";
 	../gradlew ${SETTINGS_FILE_ARGS} lint test ${GRADLE_ARGS};
 	RESULT=$?;
 	checkResult ${RESULT};
+    echo ">> Running lint & test... DONE";
 
+    echo ">> Running build & assemble...";
 	../gradlew ${SETTINGS_FILE_ARGS} build assemble ${GRADLE_ARGS};
 	RESULT=$?;
 	checkResult ${RESULT};
+	echo ">> Running build & assemble... DONE";
 fi
 
+echo ">> Running assemble release & copy-to-output dir...";
 ../gradlew ${SETTINGS_FILE_ARGS} :${DIRECTORY}:assembleRelease :${DIRECTORY}:copyReleaseApkToOutputDirs ${GRADLE_ARGS};
 RESULT=$?;
 checkResult ${RESULT};
+echo ">> Running assemble release & copy-to-output dir... DONE";
 
+echo ">> Cleaning keys...";
 ./keys_cleanup.sh;
 RESULT=$?;
 checkResult ${RESULT};
+echo ">> Cleaning keys... DONE";
 
 echo ">> Building... DONE";
 exit ${RESULT};
