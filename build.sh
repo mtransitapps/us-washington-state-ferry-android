301afeec89ada7e5b78b0585d0c93f6a4aa6c897
Mathieu Méa <mathieu.mea@gmail.com>
2018-09-20 10:36:45 -0400

    New build scripts.

--- build.sh
+++ build.sh
@@ -1,31 +1,51 @@
 #!/bin/bash
 source ../commons/commons.sh
 echo ">> Building...";
+
+DIRECTORY=$(basename ${PWD});
+CUSTOM_SETTINGS_GRADLE_FILE="../settings.gradle.all";
+
 IS_CI=false;
 if [[ ! -z "${CI}" ]]; then
 	IS_CI=true;
 fi
-echo "app-android/build.sh: IS_CI '${IS_CI}'";
+echo "$DIRECTORY/build.sh > IS_CI:'${IS_CI}'";
+
 GRADLE_ARGS="";
-if [ $IS_CI = true ]; then
+if [ ${IS_CI} = true ]; then
 	GRADLE_ARGS=" --console=plain";
 fi
-DIRECTORY=$(basename ${PWD});
-CUSTOM_SETTINGS_GRADLE_FILE="../settings.gradle.all";
+
+SETTINGS_FILE_ARGS="";
+if [ -f ${CUSTOM_SETTINGS_GRADLE_FILE} ]; then
+	SETTINGS_FILE_ARGS=" -c $CUSTOM_SETTINGS_GRADLE_FILE"; #--settings-file
+fi
+
 ./keys_setup.sh;
 RESULT=$?;
-checkResult $RESULT;
-if [ -f $CUSTOM_SETTINGS_GRADLE_FILE ]; then
-	../gradlew -c $CUSTOM_SETTINGS_GRADLE_FILE :$DIRECTORY:clean :$DIRECTORY:assembleRelease :$DIRECTORY:copyReleaseApkToOutputDirs $GRADLE_ARGS;
+checkResult ${RESULT};
+
+../gradlew ${SETTINGS_FILE_ARGS} clean ${GRADLE_ARGS};
+RESULT=$?;
+checkResult ${RESULT};
+
+if [ ${IS_CI} = true ]; then
+	../gradlew ${SETTINGS_FILE_ARGS} lint test ${GRADLE_ARGS};
 	RESULT=$?;
-	checkResult $RESULT;
-else
-	../gradlew :$DIRECTORY:clean :$DIRECTORY:assembleRelease :$DIRECTORY:copyReleaseApkToOutputDirs $GRADLE_ARGS;
+	checkResult ${RESULT};
+
+	../gradlew ${SETTINGS_FILE_ARGS} build assemble ${GRADLE_ARGS};
 	RESULT=$?;
-	checkResult $RESULT;
+	checkResult ${RESULT};
 fi
+
+../gradlew ${SETTINGS_FILE_ARGS} :${DIRECTORY}:assembleRelease :${DIRECTORY}:copyReleaseApkToOutputDirs ${GRADLE_ARGS};
+RESULT=$?;
+checkResult ${RESULT};
+
 ./keys_cleanup.sh;
 RESULT=$?;
-checkResult $RESULT;
+checkResult ${RESULT};
+
 echo ">> Building... DONE";
-exit $RESULT;
+exit ${RESULT};
